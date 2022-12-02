f999a88bf05faef65a5390ff9f7ea0b9518d08bc
jude <judesouthworth@pm.me>
2022-12-02 17:35:43 +0000

    ..

--- src/day2.rs
+++ src/day2.rs
@@ -1,23 +1,20 @@
 use aoc_runner_derive::{aoc};
 use std::mem::transmute;
 
-const X: u8 = b'X' - 1;
-const A: u8 = b'A';
-
-const MASK: u32 = 0x00570041;
-
 #[aoc(day2, part1)]
 fn part1(input_: &[u8]) -> i64 {
-    let input = [input_, b"\n"].concat();
+    let input = [input_, &[b'\n']].concat();
 
     unsafe {
-        transmute::<&[u8], &[u32]>(&input[0..input.len() / 4]).iter().map(|chr| {
-            let chrs    = transmute::<u32, [u16; 2]>((*chr & 0x00FF00FF) - MASK);
+        let res = transmute::<&[u8], &[u32]>(&input[0..input.len() / 4]).iter().fold((0, 0), |(a, b), chr| {
+            let chrs    = transmute::<u32, [u16; 2]>((*chr & 0x00FF00FF) - 0x00570041);
 
             let outcome = (chrs[0] * 2 + chrs[1]) % 3;
+            
+            (a + outcome, b + chrs[1])
+        });
 
-            outcome * 3 + chrs[1]
-        }).sum::<u16>() as i64
+        (res.0 as i64) * 3 + res.1 as i64
     }
 }
 
@@ -27,9 +24,9 @@ fn part2(input_: &[u8]) -> i64 {
 
     unsafe {
         transmute::<&[u8], &[u32]>(&input[0..input.len() / 4]).iter().map(|chr| {
-            let chrs    = transmute::<u32, [u16; 2]>((*chr & 0x00FF00FF) - 0x00580041);
+            let chrs   = transmute::<u32, [u16; 2]>((*chr & 0x00FF00FF) - 0x00580039);
 
-            let played = (chrs[0] + chrs[1] + 2) % 3;
+            let played = (chrs[0] + chrs[1]) % 3;
 
             chrs[1] * 3 + played + 1
         }).sum::<u16>() as i64
